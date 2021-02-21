c39afda4b6018ebd5f5aa2dc02a48fc2df3ecb2c
eternal198 <ikuya313198@gmail.com>
2021-02-21 21:10:48 +0900

    tracking

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
     <div className="pt-6">
       <header className="bg-white font-bold mb-10 px-8 sticky shadow-md top-0">
         <h1 className="mb-3 text-5xl">App Collection</h1>
-        <p className="text-lg text-gray-400 pb-4 tracking-wide">Why Don't You Waste Your Time ?</p>
+        <p className="text-lg text-gray-400 pb-4">Why Don't You Waste Your Time ?</p>
       </header>
       <div className="px-8">
         {data.map((data,idx) => (
